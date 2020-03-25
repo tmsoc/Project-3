5a656248be077bfa34933f43b5261df2b53c16f0
Tony Samaniego <tms.ocmp@gmail.com>
2020-03-25 12:45:54 -0700

    Commented out original code

--- Project_3.py
+++ Project_3.py
@@ -1,52 +1,56 @@
 from pathlib import Path
+from tkinter import *
+from tkinter import filedialog
+from tkinter import ttk
 import csv
 
+# --------------------ORIGINAL CODE------------------------
 
-def get_user_path() -> Path:
-    prompt = "Enter in a Path to import: "
-    while True:
-        user_path = input(prompt)
-        user_path = Path(user_path)
-        try:
-            assert user_path.exists() == True, "get_user_path() - File does not exist"
-        except:
-            print("That is not a valid Path")
-        else:
-            break
-    return user_path
+# def get_user_path() -> Path:
+#     prompt = "Enter in a Path to import: "
+#     while True:
+#         user_path = input(prompt)
+#         user_path = Path(user_path)
+#         try:
+#             assert user_path.exists() == True, "get_user_path() - File does not exist"
+#         except:
+#             print("That is not a valid Path")
+#         else:
+#             break
+#     return user_path
 
 
-def user_view_input(show_list: list) -> list:
-    # views_list = list(["Season", "Episode Number","Episode Name"])
-    views_list = list()
-    views_list.append(show_list[0])
-    prompt = (
-        "Enter the number of views in millions of views\n"
-        'Example: enter "1.86" for 1,860,000.\n'
-    )
-    print(prompt)
-    for row in show_list[1:]:
-        season_num = row[0]
-        episode_num = row[1]
-        name = row[2]
-        while True:
-            views = input(
-                f"Enter the number of view for {name}, "
-                + f"episode {episode_num}, season {season_num}: "
-            )
+# def user_view_input(show_list: list) -> list:
+#     # views_list = list(["Season", "Episode Number","Episode Name"])
+#     views_list = list()
+#     views_list.append(show_list[0])
+#     prompt = (
+#         "Enter the number of views in millions of views\n"
+#         'Example: enter "1.86" for 1,860,000.\n'
+#     )
+#     print(prompt)
+#     for row in show_list[1:]:
+#         season_num = row[0]
+#         episode_num = row[1]
+#         name = row[2]
+#         while True:
+#             views = input(
+#                 f"Enter the number of view for {name}, "
+#                 + f"episode {episode_num}, season {season_num}: "
+#             )
 
-            try:
-                views = float(views)
-            except:
-                print("Please enter a number of views\n")
-            else:
-                break
-        cont = input("Continue: ").lower()
-        views_list.append([season_num, episode_num, name, views])
-        if cont == "no":
-            break
+#             try:
+#                 views = float(views)
+#             except:
+#                 print("Please enter a number of views\n")
+#             else:
+#                 break
+#         cont = input("Continue: ").lower()
+#         views_list.append([season_num, episode_num, name, views])
+#         if cont == "no":
+#             break
 
-    return views_list
+#     return views_list
 
 
 def average_views(views_list: list) -> int:
@@ -110,25 +114,30 @@ def import_file(import_file: Path) -> list:
     return show_list
 
 
-def views_calculator():
-    in_file = get_user_path()
-    show_list = import_file(in_file)
-    views_list = user_view_input(show_list)
+# def views_calculator():
+#     in_file = get_user_path()
+#     show_list = import_file(in_file)
+#     views_list = user_view_input(show_list)
 
-    most_viewed = get_most_popular(views_list)
-    least_viewed = get_least_popular(views_list)
-    average = average_views(views_list)
+#     most_viewed = get_most_popular(views_list)
+#     least_viewed = get_least_popular(views_list)
+#     average = average_views(views_list)
 
-    ##    print(views_list[1][-1])
-    ##    print(type(views_list[1][-1]))
+#     ##    print(views_list[1][-1])
+#     ##    print(type(views_list[1][-1]))
 
-    more_views = int(most_viewed[-1]) - average
-    less_views = average - int(least_viewed[-1])
+#     more_views = int(most_viewed[-1]) - average
+#     less_views = average - int(least_viewed[-1])
 
-    print(f"{most_viewed[0]} had {more_views} million more views than average")
-    print(f"{least_viewed[0]} had {less_views} million less views than average")
+#     print(f"{most_viewed[0]} had {more_views} million more views than average")
+#     print(f"{least_viewed[0]} had {less_views} million less views than average")
+
+# --------------------END OF ORIGINAL CODE------------------------
+
+
+def main():
+    pass
 
 
 if __name__ == "__main__":
-    # does not work
-    views_calculator()
+    main()
