0d4b2e19e3763a9a12aaddfde17ba5f065e9585b
Tony Samaniego <tms.ocmp@gmail.com>
2020-03-26 21:29:57 -0700

    initial completion

--- Project_3.py
+++ Project_3.py
@@ -6,11 +6,18 @@ import csv
 
 
 # Global Variables
+# import file path
 import_file_path = Path()
+# imported csv file
 imported_shows = list()
+# index to track what show is currently being viewed
 current_show_index = 0
+# the number of items stored in imported_shows
 show_list_len = int()
-banana_state = False
+# True if the current episode has been renamed
+episode_renamed = False
+# True if a views count has been entered for any episode
+views_submitted = False
 
 
 # --------------------ORIGINAL CODE------------------------
@@ -58,7 +65,6 @@ banana_state = False
 #         views_list.append([season_num, episode_num, name, views])
 #         if cont == "no":
 #             break
-
 #     return views_list
 
 
@@ -85,6 +91,13 @@ def average_views(views_list: list) -> int:
 
 
 def get_most_popular(views_list: list) -> dict:
+    """
+    Increments through the imported shows
+    list and returns the dictionary element
+    with the most amount of views.
+
+    Rewritten to work with the dictionary list.
+    """
     # show_list = list()
     # max_num = 0.0
     # for show in views_list[1:]:
@@ -93,16 +106,24 @@ def get_most_popular(views_list: list) -> dict:
     #         show_name = show[2]
     #         show_list = [show_name, max_num]
     # return show_list
+    HAS_VIEWS = 4
     most_viewed = dict()
     max_num = 0.0
     for show in views_list:
-        if len(show) == 4 and show["Views"] > max_num:
+        if len(show) == HAS_VIEWS and show["Views"] > max_num:
             most_viewed = show
             max_num = show["Views"]
     return most_viewed
 
 
 def get_least_popular(views_list: list) -> dict:
+    """
+    Increments through the imported shows
+    list and returns the dictionary element
+    with the least amount of views.
+
+    Rewritten to work with the dictionary list.
+    """
     # show_list = [views_list[1][2], views_list[1][-1]]
     # min_num = float(views_list[1][-1])
     # for show in views_list[2:]:
@@ -111,16 +132,22 @@ def get_least_popular(views_list: list) -> dict:
     #         show_name = show[2]
     #         show_list = [show_name, min_num]
     # return show_list
+    HAS_VIEWS = 4
     least_viewed = dict()
     show_index = 0
+    # Cycles through the given list until an element with
+    # 4 items are found (has a views amount given). Stores
+    # the element in least_viewed and then then breaks.
     while show_index < len(views_list):
-        if len(views_list[show_index]) == 4:
+        if len(views_list[show_index]) == HAS_VIEWS:
             least_viewed = views_list[show_index]
             show_index += 1
             break
         show_index += 1
+    # Increment through the remaining list compairing the
+    # value of views for each elements with views.
     for show in views_list[show_index:]:
-        if len(show) == 4 and show["Views"] < least_viewed["Views"]:
+        if len(show) == HAS_VIEWS and show["Views"] < least_viewed["Views"]:
             least_viewed = show
     return least_viewed
 
@@ -141,8 +168,8 @@ def get_least_popular(views_list: list) -> dict:
 
 def import_file(import_file: Path) -> list:
     """
-    Imports the .csv path to the imported 
-    shows list
+    Imports the csv file from the path given
+    and returns a list with dictionary format
 
     Rewritten to import with a csv.DictReader
     and to store the file to a global list
@@ -180,8 +207,6 @@ def import_file(import_file: Path) -> list:
 
 
 # --------------------WIDGET FUNCTIONS------------------------
-def ___WIDGET___():
-    pass
 
 
 def enable_button(button: Button) -> None:
@@ -227,19 +252,23 @@ def set_output_read_only() -> None:
 # --------------------TEXT BOX / ENTRY FUNCTIONS------------------------
 
 
-def ___TEXT_BOX___():
-    pass
-
-
 def print_welcome_message() -> None:
     """
     Prints the welcome message to the
     output text box.
     """
-    set_output_write_enable()
-    message = 'Press "Begin" to enter a .csv file to upload\n'
-    output_text.insert(END, message)
-    set_output_read_only()
+    message = (
+        "INSTRUCTIONS:\n"
+        'Press "Begin" to enter a .csv file to upload\n\n'
+        "Enter the number of views for each episode received in millions"
+        '(e.g. 1.86 = 1,860,000) an press "Enter"\n\n'
+        'To correct the name of an episode. Press the "Rename" button, '
+        'enter the corrected name, and press "Enter"\n\n'
+        'Press "Next" to proceed to the next episode\n\n'
+        'At any time to see the results, press the "Exit List" button\n'
+    )
+    append_output_text(message)
+    output_text.see("1.0")
 
 
 def append_output_text(message: str) -> None:
@@ -248,29 +277,41 @@ def append_output_text(message: str) -> None:
     to the end of the output text box.
     """
     set_output_write_enable()
+    # appends the string to the text box
     output_text.insert(END, message)
+    # set the scroll bar to the bottom of text box
     output_text.see(END)
     set_output_read_only()
 
 
 def display_current_show():
     """
-    Displays the current show to the 
-    output text box. 
+    Displays the current show to the
+    output text box.
     """
+    HAS_VIEWS = 4
+    # gets the current episode and stores it to a variable
     show = imported_shows[current_show_index]
+    # builds the episode string
     show_output = (
         f"Episode: {show['Episode Name']}\n"
         f"Season: {show['Season']}, Episode: {show['Episode Number']}\n"
     )
-    if len(show) == 4:
+    # if the episode has views already entered
+    # append the views
+    # else, add an empty line
+    if len(show) == HAS_VIEWS:
         show_output += f"Views: {str(show['Views'])}\n"
     else:
         show_output += "\n"
+    # clear the text box and print the episode to the output text box
     clear_output_text()
     append_output_text(show_output)
-    if banana_state:
+    # if episode_renamed equals True, bananacise
+    if episode_renamed:
         bananacise()
+    # updates the progress label at bottom of GUI
+    update_progress_label()
 
 
 def clear_output_text() -> None:
@@ -288,39 +329,69 @@ def clear_entry_field() -> None:
 
 
 def print_views_results() -> None:
-    most_viewed = get_most_popular(imported_shows)
-    least_viewed = get_least_popular(imported_shows)
-    avg_views = average_views(imported_shows)
-
+    """
+    Prints a summary of the most and least
+    watched episodes to the output text box.
+    """
+    # stores the name of the program and author
     program_name = Path(__file__).name
     name = "Tony Samaniego"
-
-    most_name = most_viewed["Episode Name"]
-    most_season = most_viewed["Season"]
-    most_episode = most_viewed["Episode Number"]
-    most_popular = f"{most_name} Season {most_season} / Episode {most_episode}"
-
-    least_name = least_viewed["Episode Name"]
-    least_season = least_viewed["Season"]
-    least_episode = least_viewed["Episode Number"]
-    least_popular = f"{least_name} Season {least_season} / Episode {least_episode}"
-
-    more_than = most_viewed["Views"] - avg_views
-    less_than = avg_views - least_viewed["Views"]
-
+    # builds the string to output
     report_string = (
-        f"{program_name}\n"
-        f"------------------\n"
-        f"{name}\n"
-        "------------------\n"
-        f"The average number of views for all episodes so far is {avg_views} views. The most popular episode was {most_popular} with {more_than:.1f} more than average. The least popular episode was {least_popular} with {less_than:.1f} less than average"
+        f"""{program_name}\n"""
+        """----------------------\n"""
+        f"""{name}\n"""
+        """----------------------\n"""
     )
+    # If views were entered for any of the episodes,
+    # append the results to the output string.
+    # Else, append that no views were entered to the output string.
+    if views_submitted:
+        # gets the most viewed episode
+        most_viewed = get_most_popular(imported_shows)
+        # gets the least viewed episode
+        least_viewed = get_least_popular(imported_shows)
+        # gets the average amount of views
+        avg_views = average_views(imported_shows)
+
+        most_popular = (
+            f"""{most_viewed["Episode Name"]} Season """
+            f"""{most_viewed["Season"]} / Episode """
+            f"""{most_viewed["Episode Number"]}"""
+        )
+
+        least_popular = (
+            f"""{least_viewed["Episode Name"]} Season """
+            f"""{least_viewed["Season"]} / Episode """
+            f"""{least_viewed["Episode Number"]}"""
+        )
+
+        more_than = most_viewed["Views"] - avg_views
+        less_than = avg_views - least_viewed["Views"]
+
+        report_string += (
+            f"""The average number of views for all episodes so far is """
+            f"""{avg_views} views. The most popular episode was """
+            f"""{most_popular} with {more_than:.2f} more than average. """
+            f"""The least popular episode was {least_popular} with """
+            f"""{less_than:.2f} less than average\n"""
+        )
+    else:
+        report_string += "No views entered to report\n"
     append_output_text(report_string)
 
 
+def update_progress_label() -> None:
+    """
+    Updates the progress label on what
+    episode is currently being viewed
+    at the bottom of the GUI.
+    """
+    progress = f"Show {current_show_index + 1} of {show_list_len}"
+    progress_label["text"] = progress
+
+
 # --------------------UI FUNCTIONS------------------------
-def ___UI___():
-    pass
 
 
 def init_ui() -> None:
@@ -349,6 +420,19 @@ def init_viewer_buttons() -> None:
     insert_widget(next_button)
     insert_widget(exit_button)
     insert_widget(rename_button)
+
+
+def init_rename_buttons() -> None:
+    """
+    Sets the button states on the
+    GUI for renaming an episode
+    """
+    remove_widget(enter_views_button)
+    insert_widget(enter_name_button)
+    disable_button(rename_button)
+    disable_button(next_button)
+    disable_button(exit_button)
+    entry_label["text"] = "New Name:"
 
 
 def reset_viewer_buttons() -> None:
@@ -381,16 +465,13 @@ def bananacise() -> None:
 
 
 # --------------------IMPORT / EXPORT FUNCTIONS------------------------
-def ___IMPORT___():
-    pass
 
 
 def get_import_file() -> Path:
     """
-    Returns a Path to the selected file
-    by the user. Opens a filedialog 
-    window to the user to select a file 
-    to import.
+    Opens a filedialog window to the
+    user to select a file to import.
+    Returns a Path to the selected file.
     """
     return Path(filedialog.askopenfilename())
 
@@ -410,7 +491,7 @@ def import_csv_file(import_path: Path) -> None:
 def export_shows_list() -> None:
     """
     Exports the updated imported shows list
-    to the program file directory. The file 
+    to the program file directory. The file
     name will have the same name as the file
     imported with "_views" added to the file
     name.
@@ -422,7 +503,7 @@ def export_shows_list() -> None:
     export_file = file_name + file_tag + file_type
     # creates the file and uploads the data to it in csv format
     with open(export_file, "w", newline="") as out_file:
-        # Gets the header from the dictionary keys
+        # Gets the headers from the dictionary keys
         keys = get_dictionary_keys()
         writer = csv.DictWriter(out_file, fieldnames=keys)
         # writes the file header
@@ -433,8 +514,6 @@ def export_shows_list() -> None:
 
 
 # --------------------SEQUENCE FUNCTIONS-------------------------------
-def ___SEQUENCE___():
-    pass
 
 
 def init_shows_viewer() -> None:
@@ -472,7 +551,7 @@ def get_view_count() -> float:
         if view_cnt < 0:
             raise Exception
     except:
-        # If not valid, user is prompted
+        # If not valid, prompt the user
         append_output_text(f'Error - Invalid Entry "{view_cnt}"\n')
         view_cnt = -1
     return view_cnt
@@ -480,7 +559,7 @@ def get_view_count() -> float:
 
 def store_view_count(count: float) -> None:
     """
-    Stores the given number of views 
+    Stores the given number of views
     to the current show. number of views
     is stored with a dictionary key of
     'Views'
@@ -492,31 +571,32 @@ def get_dictionary_keys() -> list:
     """
     Returns a list of the keys for the
     imported shows list. Checks the list
-    for the correct number of keys by 
+    for the correct number of keys by
     cycles through and verifying that "Views"
-    should be included with the list of 
+    should be included with the list of
     keys.
     """
     MAX_KEYS = 4
-    # Gets the default 3 set of keys
+    # Gets a default set of keys
     keys = imported_shows[0].keys()
-    for item in imported_shows:
-        # if an item in the list has 4 elements
-        if len(item) == MAX_KEYS:
+    # if the amount of keys doesn't equal 4
+    # search the list for an element with 4 keys
+    if len(keys) != MAX_KEYS:
+        for item in imported_shows:
+            # if an item in the list has 4 elements
             # get a list of its keys an break
-            keys = item.keys()
-            break
+            if len(item) == MAX_KEYS:
+                keys = item.keys()
+                break
     return keys
 
 
 # --------------------BUTTON FUNCTIONS-------------------------------
-def ___BUTTON___():
-    pass
 
 
 def begin_button_press() -> None:
     """
-    Begins the shows views sequence once
+    Begins the episode views sequence once
     the user selects a valid .csv file.
     If an invalid file is selected, user
     is notified on the output text box
@@ -526,10 +606,10 @@ def begin_button_press() -> None:
     upload_file = get_import_file()
     # Verifies that the file has a .csv suffix
     if upload_file.is_file() and upload_file.suffix == ".csv":
-        # Saves the file path as a global variable
+        # Saves the file path to a global variable
         global import_file_path
         import_file_path = upload_file
-        # imports the shows list
+        # imports the episode list
         import_csv_file(import_file_path)
         # initializes the GUI for entering shows viewings
         init_shows_viewer()
@@ -553,6 +633,8 @@ def enter_views_button_press() -> None:
     if view_cnt != -1:
         store_view_count(view_cnt)
         display_current_show()
+        global views_submitted
+        views_submitted = True
     clear_entry_field()
 
 
@@ -561,20 +643,14 @@ def rename_button_press() -> None:
     Begins the sequence to replace the
     name for the current show
     """
-    # initializes the UI buttons and labels
-    remove_widget(enter_views_button)
-    insert_widget(enter_name_button)
-    disable_button(rename_button)
-    disable_button(next_button)
-    disable_button(exit_button)
-    entry_label["text"] = "New Name:"
+    init_rename_buttons()
 
 
 def enter_name_button_press():
     """
     Stores the entered name to the show,
-    resets the all the UI buttons to 
-    viewer settings, and bananacise the UI.
+    resets the all the UI buttons to
+    viewer settings, and bananacises the UI.
     """
     # Gets the new name from the entry field
     new_name = entry_field.get()
@@ -590,60 +666,79 @@ def enter_name_button_press():
         # the output text box
         imported_shows[current_show_index]["Episode Name"] = new_name
         # displays banana
-        global banana_state
-        banana_state = True
+        global episode_renamed
+        episode_renamed = True
         display_current_show()
 
 
 def next_button_press():
     """
-    Increments to the next show in the 
-    shows list. 
+    Increments to the next episode in the
+    shows list.
     """
-    global banana_state
+    global episode_renamed
     global current_show_index
-    banana_state = False
+    episode_renamed = False
     current_show_index += 1
+    # if the last episode was been viewed,
+    # end the enter views sequence
     if current_show_index == show_list_len:
         exit_button_press()
+    # else, reset the UI buttons and display the next show
     else:
         init_viewer_buttons()
         display_current_show()
 
 
 def exit_button_press():
-    """ """
+    """
+    Ends the views entry sequence and
+    prints a result summary to the
+    output text box.
+    """
+    # changes the progress label to dispaly "Show Summary"
+    progress_label["text"] = "Show Summary"
+    # disables all buttons
     disable_views_buttons()
+    # prints the views results to the output text box
     clear_output_text()
     print_views_results()
+    # exports the modified imported_shows list to a new csv file
     export_shows_list()
 
 
-# Main Window
+# Main GUI Window
 root = Tk()
 root.title("Lively Earth Studios")
 root.iconbitmap("PyCrust.ico")
-# Grid Layout
+root.geometry("540x182")
+root.resizable(0, 0)
+
+# Widget Items
+# Text
 content = ttk.Frame(root, padding=(10, 10, 10, 10))
 text_frame = Frame(content, relief="groove")
 text_scrollbar = Scrollbar(text_frame)
+progress_label = ttk.Label(content, text="    ")
 entry_label = ttk.Label(content, text="Views")
 entry_field = ttk.Entry(content)
 output_text = Text(
     text_frame,
     yscrollcommand=text_scrollbar.set,
-    width=35,
+    width=40,
     height=8,
-    font=("Helvetica", 12),
+    font=("Helvetica", 11),
     wrap=(WORD),
     state="disabled",
 )
 # Buttons
 begin_button = ttk.Button(content, text="Begin", command=begin_button_press)
 enter_views_button = ttk.Button(
-    content, text="Enter1", command=enter_views_button_press
+    content, text="Enter", command=enter_views_button_press
 )
-enter_name_button = ttk.Button(content, text="Enter2", command=enter_name_button_press)
+enter_name_button = ttk.Button(
+    content, text="Enter", command=enter_name_button_press
+)
 rename_button = ttk.Button(content, text="Rename", command=rename_button_press)
 next_button = ttk.Button(content, text="Next", command=next_button_press)
 exit_button = ttk.Button(content, text="Exit List", command=exit_button_press)
@@ -651,12 +746,14 @@ exit_button = ttk.Button(content, text="Exit List", command=exit_button_press)
 banana = PhotoImage(file="bananadance.gif")
 small_banana = banana.subsample(2)
 banana_label = Label(content, image=small_banana)
-# Wiget Placement
+
+# Widget Placement
 content.grid(column=0, row=0, sticky=(N, S, E, W))
 text_frame.grid(column=0, row=0, columnspan=3, rowspan=5, sticky=(N, S, E, W))
 text_scrollbar.grid(row=0, column=1, sticky=(N, S, E))
 text_scrollbar.config(command=output_text.yview)
 output_text.grid(row=0, column=0, sticky=(N, S, W, E))
+progress_label.grid(row=6, column=0, sticky=(N, W))
 entry_label.grid(column=3, row=0, columnspan=2, sticky=(N, W), padx=5)
 entry_field.grid(column=3, row=1, columnspan=2, sticky=(N, E, W), padx=5)
 begin_button.grid(column=3, row=2, padx=5, pady=3, sticky=(N))
@@ -666,6 +763,7 @@ rename_button.grid(column=4, row=2, padx=5)
 next_button.grid(column=3, row=3, padx=5, pady=6)
 exit_button.grid(column=3, row=4, padx=5, pady=3)
 banana_label.grid(column=4, row=3, rowspan=3)
+
 # GUI Formatting
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
@@ -678,6 +776,7 @@ content.rowconfigure(1, weight=1)
 
 
 def main():
+    # initializes and runs the program GUI
     init_ui()
     root.mainloop()
 
